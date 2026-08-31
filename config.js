9c1e44ff6245c9593125ed81d14bfce139292d3a
JOFAMS <130027524+kcbdc@users.noreply.github.com>
2026-09-01 08:08:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 // Firebase 웹 설정 객체는 클라이언트 식별용 값이며, 실제 데이터 접근 권한은 Firestore Rules로 통제합니다.
 window.__APP_CONFIG__ = {
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyBdzxgVus4YV1KB8JiKV-DX3IiR9gDEVnY",
+    authDomain: "jofams-smartdrive.firebaseapp.com",
+    projectId: "jofams-smartdrive",
+    storageBucket: "jofams-smartdrive.firebasestorage.app",
+    messagingSenderId: "681909120668",
+    appId: "1:681909120668:web:0057cb31181df86b07c759"
   }
 };
